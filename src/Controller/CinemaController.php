973aa4e8726bb3e0dc25d8f202a5bcb88ee3f1ee
VvAlexandrevV <luis.alexandre@outlook.fr>
2026-03-11 15:27:58 +0100

    reservation annulation seance

--- src/Controller/CinemaController.php
+++ src/Controller/CinemaController.php
@@ -5,7 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use App\Repository\FilmRepository;
+use App\Entity\Reservation;
+use App\Entity\Seance;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ReservationType;
 
 final class CinemaController extends AbstractController
 {
@@ -14,5 +21,50 @@ final class CinemaController extends AbstractController
     {
         $films = $filmRepository->findAll();
         return $this->render('cinema/programmation.html.twig', ['films' => $films]);
+    }
+    //si authent ok alors acces aux reservation du profil
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/profil', name: 'app_profil')]
+
+    public function profil(): Response {
+    
+        return $this->render('cinema/reservations.html.twig');
+    }
+    //si authent ok alors acces aux reservation du profil
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/annuler-reservation/{id}', name: 'app_annuler_reservation')]
+
+    public function annulerReservation(Reservation $reservation, EntityManagerInterface $entityManager): Response {
+
+        //est ce que l id de l utilisateur de reservation a l id de l utlisateur connecté si c est === on supprime , sinon non
+        if($reservation->getUtilisateur()->getId() === $this->getUser()->getId()) {
+            $reservation->setStatut(Reservation::STATUT_ANNULE);//on supprime pas , on annule comme ca on garde l historique
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('app_profil');
+    }
+
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/reserver/{id}', name: 'app_reserver')]
+    public function reserver(Seance $seance, Request $request, EntityManagerInterface $entityManager): Response {
+
+        $reservation = new Reservation();
+
+        $form = $this->createForm(ReservationType::class, $reservation);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $reservation
+                ->setUtilisateur($this->getUser())
+                ->setSeance($seance)
+                ->setStatut(Reservation::STATUT_CONFIRME);
+
+            $entityManager->persist($reservation);
+            $entityManager->flush(); 
+            
+            return $this->redirectToRoute('app_profil');
+        }
+        return $this->render('cinema/reserver.html.twig', ['seance' => $seance, 'form' => $form->createView()]);
     }
 }
---
+++ src/Form/ReservationType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reservation;
+use App\Entity\Seance;
+use App\Entity\utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nombre_place')  
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+        ]);
+    }
+}
